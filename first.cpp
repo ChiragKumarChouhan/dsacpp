b6b12c165472b80a025dd777f315ba3a2c6f1742
ChiragKumarChouhan <chiragneeraj2@gmail.com>
2025-11-27 23:16:51 +0530

    initial commit

--- first.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-
-    cout<<"chirag\nkuamr"<<endl;
-
-}
--- ternarystatement.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int n ;
-    cout<<"enter n:";
-    cin>>n;
-    cout<<(n>=0?"positive":"negative")<<endl;
-    return 0;
-}
